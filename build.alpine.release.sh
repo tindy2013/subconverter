46b78ce250196ebbd8af4c85f9371b61be4f85c6
Tindy X <49061470+tindy2013@users.noreply.github.com>
2019-11-08 14:25:08 +0800

    Update script

--- build.alpine.release.sh
+++ build.alpine.release.sh
@@ -3,12 +3,12 @@ mkdir obj
 set -xe
 
 apk add gcc g++ build-base linux-headers cmake make autoconf automake libtool
-apk add libressl-dev zlib-dev rapidjson-dev
+apk add libressl-dev zlib-dev rapidjson-dev libevent-dev libevent-static
 
 git clone https://github.com/curl/curl
 cd curl
 ./buildconf
-./configure > /dev/null
+./configure --with-ssl --disable-ldap --disable-ldaps --disable-rtsp --without-libidn2 > /dev/null
 make install -j4
 cd ..
 
@@ -23,15 +23,15 @@ cd bzip2
 make install -j4
 cd ..
 
-g++ -Wall -std=c++1z -fexceptions -DCURL_STATICLIB -c logger.cpp -o obj\logger.o
-g++ -Wall -std=c++1z -fexceptions -DCURL_STATICLIB -c main.cpp -o obj\main.o
-g++ -Wall -std=c++1z -fexceptions -DCURL_STATICLIB -c misc.cpp -o obj\misc.o
-g++ -Wall -std=c++1z -fexceptions -DCURL_STATICLIB -c nodemanip.cpp -o obj\nodemanip.o
-g++ -Wall -std=c++1z -fexceptions -DCURL_STATICLIB -c rapidjson_extra.cpp -o obj\rapidjson_extra.o
-g++ -Wall -std=c++1z -fexceptions -DCURL_STATICLIB -c speedtestutil.cpp -o obj\speedtestutil.o
-g++ -Wall -std=c++1z -fexceptions -DCURL_STATICLIB -c subexport.cpp -o obj\subexport.o
-g++ -Wall -std=c++1z -fexceptions -DCURL_STATICLIB -c webget.cpp -o obj\webget.o
-g++ -Wall -std=c++1z -fexceptions -DCURL_STATICLIB -c webserver_libevent.cpp -o obj\webserver_libevent.o
+g++ -Wall -std=c++17 -fexceptions -DCURL_STATICLIB -c logger.cpp -o obj\logger.o
+g++ -Wall -std=c++17 -fexceptions -DCURL_STATICLIB -c main.cpp -o obj\main.o
+g++ -Wall -std=c++17 -fexceptions -DCURL_STATICLIB -c misc.cpp -o obj\misc.o
+g++ -Wall -std=c++17 -fexceptions -DCURL_STATICLIB -c nodemanip.cpp -o obj\nodemanip.o
+g++ -Wall -std=c++17 -fexceptions -DCURL_STATICLIB -c rapidjson_extra.cpp -o obj\rapidjson_extra.o
+g++ -Wall -std=c++17 -fexceptions -DCURL_STATICLIB -c speedtestutil.cpp -o obj\speedtestutil.o
+g++ -Wall -std=c++17 -fexceptions -DCURL_STATICLIB -c subexport.cpp -o obj\subexport.o
+g++ -Wall -std=c++17 -fexceptions -DCURL_STATICLIB -c webget.cpp -o obj\webget.o
+g++ -Wall -std=c++17 -fexceptions -DCURL_STATICLIB -c webserver_libevent.cpp -o obj\webserver_libevent.o
 g++ -o subconverter obj\logger.o obj\main.o obj\misc.o obj\nodemanip.o obj\rapidjson_extra.o obj\speedtestutil.o obj\subexport.o obj\webget.o obj\webserver_libevent.o  -static -levent -lyaml-cpp -lcurl -lssl -lcrypto -lz -lbz2 -ldl -lpthread -O3 -s  
 
 chmod +rx subconverter pref.ini *.yml
