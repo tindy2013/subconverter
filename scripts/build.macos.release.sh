38314a778b5fc066bfb414a9afe5b31579c4b2ce
Tindy X <49061470+tindy2013@users.noreply.github.com>
2020-03-18 19:48:41 +0800

    Update build scripts

--- scripts/build.macos.release.sh
+++ scripts/build.macos.release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-brew reinstall rapidjson libevent zlib pcre2 bzip2 pkgconfig
+brew reinstall rapidjson libevent zlib pcre2 bzip2 libssh2 pkgconfig
 
 git clone https://github.com/curl/curl
 cd curl
@@ -25,18 +25,18 @@ cp /usr/local/opt/openssl@1.1/lib/libssl.a .
 cp /usr/local/opt/openssl@1.1/lib/libcrypto.a .
 cp /usr/local/lib/libpcre2-8.a .
 cp /usr/local/opt/bzip2/lib/libbz2.a .
+cp /usr/local/lib/libssh2.a .
 
 export CMAKE_CXX_FLAGS="-I/usr/local/include -I/usr/local/opt/openssl@1.1/include -I/usr/local/opt/curl/include"
 cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1 .
 make -j8
 rm subconverter
-c++ -Xlinker -unexported_symbol -Xlinker "*" -o base/subconverter CMakeFiles/subconverter.dir/src/*.o libpcre2-8.a libevent.a libcurl.a libz.a libssl.a libcrypto.a libyaml-cpp.a libbz2.a -ldl -lpthread -O3
+c++ -Xlinker -unexported_symbol -Xlinker "*" -o base/subconverter CMakeFiles/subconverter.dir/src/*.o libpcre2-8.a libevent.a libcurl.a libz.a libssl.a libcrypto.a libyaml-cpp.a libbz2.a libssh2.a -ldl -lpthread -O3
 
 cd base
 chmod +rx subconverter
 chmod +r *
 cd ..
 mv base subconverter
-tar czf subconverter_darwin64.tar.gz subconverter
 
 set +xe
