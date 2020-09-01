24267087cb2e3c30cc63d48db6068a258f06e4e9
Tindy X <49061470+tindy2013@users.noreply.github.com>
2020-09-01 22:12:57 +0800

    Fix subsystem on Actions
    Fix install prefix on Travis CI.

--- scripts/build.windows.release.sh
+++ scripts/build.windows.release.sh
@@ -3,13 +3,13 @@ set -xe
 
 git clone https://github.com/curl/curl --depth=1
 cd curl
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_USE_LIBSSH2=OFF -DHTTP_ONLY=ON -DCMAKE_USE_SCHANNEL=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_CURL_EXE=OFF -G "Unix Makefiles" .
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_USE_LIBSSH2=OFF -DHTTP_ONLY=ON -DCMAKE_USE_SCHANNEL=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_CURL_EXE=OFF -DCMAKE_INSTALL_PREFIX=/usr -G "Unix Makefiles" .
 make install -j4
 cd ..
 
 git clone https://github.com/jbeder/yaml-cpp --depth=1
 cd yaml-cpp
-cmake -DCMAKE_BUILD_TYPE=Release -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=OFF -G "Unix Makefiles" .
+cmake -DCMAKE_BUILD_TYPE=Release -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=OFF -DCMAKE_INSTALL_PREFIX=/usr -G "Unix Makefiles" .
 make install -j4
 cd ..
 
@@ -30,7 +30,7 @@ cd ../../../..
 
 git clone https://github.com/Tencent/rapidjson --depth=1
 cd rapidjson
-cmake -DRAPIDJSON_BUILD_DOC=OFF -DRAPIDJSON_BUILD_EXAMPLES=OFF -DRAPIDJSON_BUILD_TESTS=OFF -G "Unix Makefiles" .
+cmake -DRAPIDJSON_BUILD_DOC=OFF -DRAPIDJSON_BUILD_EXAMPLES=OFF -DRAPIDJSON_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=/usr -G "Unix Makefiles" .
 make install -j4
 cd ..
 
