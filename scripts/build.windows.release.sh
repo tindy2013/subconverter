e668cde4e085bc37262d66abff02c80776fb0706
Tindy X <49061470+tindy2013@users.noreply.github.com>
2020-09-18 13:56:49 +0800

    Use Node JS for building duktape library

--- scripts/build.windows.release.sh
+++ scripts/build.windows.release.sh
@@ -15,10 +15,10 @@ cd ..
 
 git clone https://github.com/svaarala/duktape --depth=1
 cd duktape
-pip2 install PyYAML
-mkdir dist
+make -C src-tools
 python2 util/dist.py
-cd dist/source/src
+node src-tools/index.js dist --output-directory dist
+cd dist/src
 gcc -c -O3 -o duktape.o duktape.c
 gcc -c -O3 -o duk_module_node.o -I. ../extras/module-node/duk_module_node.c
 ar cr libduktape.a duktape.o
@@ -26,7 +26,7 @@ ar cr libduktape_module.a duk_module_node.o
 install -m0644 ./*.a "$MINGW_PREFIX/lib"
 install -m0644 ./duk*.h "$MINGW_PREFIX/include"
 install -m0644 ../extras/module-node/duk_module_node.h "$MINGW_PREFIX/include"
-cd ../../../..
+cd ../../..
 
 git clone https://github.com/Tencent/rapidjson --depth=1
 cd rapidjson
