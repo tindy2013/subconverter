ab4d7543cec46cd9e45680f3b43d82de16f38a3d
Tindy X <49061470+tindy2013@users.noreply.github.com>
2020-09-20 01:06:01 +0800

    Fix Duktape library not found

--- cmake/FindDuktape.cmake
+++ cmake/FindDuktape.cmake
@@ -6,7 +6,7 @@ find_library(DUKTAPE_MODULE_LIBRARY duktape_module)
 set(DUKTAPE_LIBRARIES "${DUKTAPE_LIBRARY}" "${DUKTAPE_MODULE_LIBRARY}")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(DUKTAPE DEFAULT_MSG
+find_package_handle_standard_args(Duktape DEFAULT_MSG
     DUKTAPE_INCLUDE_DIRS DUKTAPE_LIBRARY DUKTAPE_MODULE_LIBRARY)
 
 mark_as_advanced(DUKTAPE_INCLUDE_DIRS DUKTAPE_LIBRARY DUKTAPE_MODULE_LIBRARY)
