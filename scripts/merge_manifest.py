47f8e35565be01d3c95925f26542923750679f59
Tindy X <49061470+tindy2013@users.noreply.github.com>
2022-08-24 04:21:58 +0800

    Fix build error
    Use self-hosted machine for ARM docker build.

--- scripts/merge_manifest.py
+++ scripts/merge_manifest.py
@@ -7,7 +7,7 @@ TARGET_TAG="latest" if len(sys.argv) < 2 else sys.argv[1]
 args=["docker manifest create {}:{}".format(MAIN_IMAGE_NAME, TARGET_TAG)]
 for i in glob.glob("/tmp/images/*/*.txt"):
     with open(i, "r") as file:
-        args += " --amend {}@{}".format(MAIN_IMAGE_NAME, file.readline())
+        args += " --amend {}@{}".format(MAIN_IMAGE_NAME, file.readline().strip())
 cmd_create="".join(args)
 cmd_push="docker manifest push {}:{}".format(MAIN_IMAGE_NAME, TARGET_TAG)
 os.system(cmd_create)
