1b18bcab083fe7f62ee4eee7bbae8c0b172edfbb
Tindy X <49061470+tindy2013@users.noreply.github.com>
2023-10-03 22:41:44 +0800

    Add auto-sync script for rules
    Update build scripts.

--- scripts/build.alpine.release.sh
+++ scripts/build.alpine.release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-apk add gcc g++ build-base linux-headers cmake make autoconf automake libtool python2
+apk add gcc g++ build-base linux-headers cmake make autoconf automake libtool python2 python3
 apk add mbedtls-dev mbedtls-static zlib-dev rapidjson-dev libevent-dev libevent-static zlib-static pcre2-dev
 
 git clone https://github.com/curl/curl --depth=1 --branch curl-7_88_1
@@ -45,6 +45,10 @@ cmake -DCMAKE_BUILD_TYPE=Release .
 make -j2
 rm subconverter
 g++ -o base/subconverter $(find CMakeFiles/subconverter.dir/src/ -name "*.o")  -static -lpcre2-8 -levent -lyaml-cpp -L/usr/lib64 -lcurl -lmbedtls -lmbedcrypto -lmbedx509 -lz -l:quickjs/libquickjs.a -llibcron -O3 -s  
+
+python3 -m ensurepip
+python3 -m pip install gitpython
+python3 scripts/update_rules.py -c scripts/rules_config.conf
 
 cd base
 chmod +rx subconverter
