7b54498e56982d7087fbba88a693e4438ff85d52
Lai Zenan <laizenan@gmail.com>
2024-09-09 04:25:16 +0800

    fix: remove fileGet in subexport.cpp for security issue

--- src/generator/config/subexport.cpp
+++ src/generator/config/subexport.cpp
@@ -2370,7 +2370,7 @@ void proxyToSingBox(std::vector<Proxy> &nodes, rapidjson::Document &json, std::v
                 if (!x.Ca.empty())
                 {
                     rapidjson::Value ca_str;
-                    ca_str.SetString(fileGet(x.Ca).c_str(), allocator);
+                    ca_str.SetString(x.Ca.c_str(), allocator);
                     tls.AddMember("certificate", ca_str, allocator);
                 }
                 if (!x.CaStr.empty())
@@ -2409,7 +2409,7 @@ void proxyToSingBox(std::vector<Proxy> &nodes, rapidjson::Document &json, std::v
                 if (!x.Ca.empty())
                 {
                     rapidjson::Value ca_str(rapidjson::kStringType);
-                    ca_str.SetString(fileGet(x.Ca).c_str(), allocator);
+                    ca_str.SetString(x.Ca.c_str(), allocator);
                     tls.AddMember("certificate", ca_str, allocator);
                 }
                 if (!x.CaStr.empty())
